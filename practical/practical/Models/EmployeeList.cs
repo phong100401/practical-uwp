5f8e475c7e49ee51798228a9d7bf4d8d233e1036
phong100401 <65002403+phong100401@users.noreply.github.com>
2021-06-14 20:41:43 +0700

    practical
    practical

---
+++ practical/practical/Models/EmployeeList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practical.Models
+{
+    public class EmployeeList
+    {
+        public string name { get; set; }
+
+        public string role { get; set; }
+
+        public string birthyear { get; set; }
+    }
+
+    public class Root
+    {
+        public List<EmployeeList> employeeLists { get; set; }
+    }
+}
